20340adcd4064c5e299765fb44a983fc38df83e8
Tristan Bond <tristanebond@gmail.com>
2021-03-14 15:02:50 -0700

    schema setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import "bootstrap/scss/bootstrap-reboot.scss"
+import "bootstrap/scss/bootstrap-grid.scss"
 
-class App extends Component {
-  render() {
+
+function App () {
+  
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+         
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
@@ -15,7 +15,7 @@ class App extends Component {
         </p>
       </div>
     );
-  }
+  
 }
 
 export default App;
